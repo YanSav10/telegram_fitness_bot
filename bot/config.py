308087bbffde2850b0f3f266f758a53cdba18ecf
Yan Savinov <savinovyan@gmail.com>
2025-05-13 11:16:18 +0300

    fix: load Firebase credentials from env

--- bot/config.py
+++ bot/config.py
@@ -1,19 +1,20 @@
 import os
-from pathlib import Path
-from dotenv import load_dotenv
 import json
+from dotenv import load_dotenv
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 ENV_PATH = BASE_DIR / ".env"
 if ENV_PATH.exists():
     load_dotenv(ENV_PATH)
 
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN") or os.getenv("BOT_TOKEN")
+# Токен бота
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-FIREBASE_KEY_PATH = BASE_DIR / "firebase_key.json"
-
-firebase_json = os.getenv("FIREBASE_CREDENTIALS_JSON")
+# Отримуємо JSON-ключ як рядок зі змінної середовища
+firebase_json = os.getenv("FIREBASE_KEY_PATH")
 
 if firebase_json:
-    with open(FIREBASE_KEY_PATH, "w") as f:
-        json.dump(json.loads(firebase_json), f)
+    FIREBASE_CREDENTIALS = json.loads(firebase_json)
+else:
+    raise ValueError("❌ Firebase credentials not found in FIREBASE_KEY_PATH")
